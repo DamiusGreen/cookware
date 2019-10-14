f057953288d86570f35fc5308d8c184275124976
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 16:40:22 -0400

    Updates

---
+++ precache-manifest.3be9998b80b9e1fbd2eb7530b2dd10fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bbaf6ff8562b217849ce321791b5180",
+    "url": "/cookware/index.html"
+  },
+  {
+    "revision": "3154af8d92957953b512",
+    "url": "/cookware/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/cookware/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "3154af8d92957953b512",
+    "url": "/cookware/static/js/main.dcc5458d.chunk.js"
+  },
+  {
+    "revision": "7c3a828fc671b7b10324",
+    "url": "/cookware/static/js/runtime~main.63188c1d.js"
+  }
+]);
